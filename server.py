593e57a0805ca52387246aeb98ba8a51b763c284
Rennan Haro <rennanharo@rhmbp.local>
2021-02-13 17:54:30 -0300

    First commit

---
+++ server.py
@@ -0,0 +1,53 @@
+import hashlib
+import json
+from textwrap import dedent
+from time import time
+from uuid import uuid4
+
+from flask import Flask, jsonify
+
+from blockchain import Blockchain
+
+# Instantiate our Nodde
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace("-", "")
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+
+@app.route("/mine", methods=["GET"])
+def mine():
+    return "We'll mine a new Block"
+
+
+@app.route("/transactions/new", methods=["POST"])
+def new_transaction(request):
+    values = request.get_json()
+
+    # Check that the required fields are in the POSTed data
+    required = ["sender", "recipient", "amount"]
+
+    # TODO Return description of values that are missing
+    if not all(k in values for k in required):
+        return "Missing values", 400
+
+    # Create a new transaction
+    index = blockchain.new_transaction(
+        values["sender"], values["recipient"], values["amount"]
+    )
+
+    response = {"message": f"Transaction will be added to Block {index}"}
+    return jsonify(response), 201
+
+
+@app.route("/chain", methods=["GET"])
+def full_chain():
+    response = {"chain": blockchain.chain, "length": len(blockchain.chain)}
+    return jsonify(response), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
