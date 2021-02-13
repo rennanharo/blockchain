e138407239360678407230d5c5d9e2a4077bcf77
= <=>
2021-02-13 18:54:00 -0300

    Registering /nodes/resolve endpoint

--- server.py
+++ server.py
@@ -72,7 +72,7 @@ def full_chain():
     return jsonify(response), 200
 
 
-@app.route("/nodes/register")
+@app.route("/nodes/register", methods=["POST"])
 def register_nodes():
     values = request.get_json()
 
@@ -89,6 +89,18 @@ def register_nodes():
     }
 
     return jsonify(response), 201
+
+
+@app.route("/nodes/resolve", methods=["GET"])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {"message": "Our chain was replaced", "new_chain": blockchain.chain}
+    else:
+        response = {"message": "Our chain is authoritative", "chain": blockchain.chain}
+
+    return jsonify(response), 200
 
 
 if __name__ == "__main__":
